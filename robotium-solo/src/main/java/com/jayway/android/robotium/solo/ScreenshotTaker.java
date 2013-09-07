5b933a22efc222dd01781eeff4c8101141a5c760
jbenf <julianfeja@gmail.com>
2013-09-07 20:19:06 +0200

    allow custom directory for ScreenshotTaker.java
    Signed-off-by: jbenf <julianfeja@gmail.com>

--- robotium-solo/src/main/java/com/jayway/android/robotium/solo/ScreenshotTaker.java
+++ robotium-solo/src/main/java/com/jayway/android/robotium/solo/ScreenshotTaker.java
@@ -6,6 +6,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.concurrent.CountDownLatch;
+
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Picture;
@@ -21,8 +22,9 @@ import android.view.View;
 import android.webkit.WebView;
 
 /**
- * Contains screenshot methods like: takeScreenshot(final View, final String name), startScreenshotSequence(final String name, final int quality, final int frameDelay, final int maxFrames), 
- * stopScreenshotSequence().
+ * Contains screenshot methods like: takeScreenshot(final View, final String
+ * name), startScreenshotSequence(final String name, final int quality, final
+ * int frameDelay, final int maxFrames), stopScreenshotSequence().
  * 
  * 
  * @author Renas Reda, renasreda@gmail.com
@@ -39,13 +41,15 @@ class ScreenshotTaker {
 	private final ViewFetcher viewFetcher;
 	private final Sleeper sleeper;
 
-
 	/**
 	 * Constructs this object.
 	 * 
-	 * @param activityUtils the {@code ActivityUtils} instance
-	 * @param viewFetcher the {@code ViewFetcher} instance
-	 * @param sleeper the {@code Sleeper} instance
+	 * @param activityUtils
+	 *            the {@code ActivityUtils} instance
+	 * @param viewFetcher
+	 *            the {@code ViewFetcher} instance
+	 * @param sleeper
+	 *            the {@code Sleeper} instance
 	 * 
 	 */
 	ScreenshotTaker(ActivityUtils activityUtils, ViewFetcher viewFetcher, Sleeper sleeper) {
@@ -55,50 +59,61 @@ class ScreenshotTaker {
 	}
 
 	/**
-	 * Takes a screenshot and saves it in "/sdcard/Robotium-Screenshots/". 
-	 * Requires write permission (android.permission.WRITE_EXTERNAL_STORAGE) in AndroidManifest.xml of the application under test.
+	 * Takes a screenshot and saves it in "/sdcard/Robotium-Screenshots/".
+	 * Requires write permission (android.permission.WRITE_EXTERNAL_STORAGE) in
+	 * AndroidManifest.xml of the application under test.
 	 * 
-	 * @param view the view to take screenshot of
-	 * @param name the name to give the screenshot image
-	 * @param quality the compression rate. From 0 (compress for lowest size) to 100 (compress for maximum quality).
+	 * @param directory
+	 *            where to save the file
+	 * @param name
+	 *            the name to give the screenshot image
+	 * @param quality
+	 *            the compression rate. From 0 (compress for lowest size) to 100
+	 *            (compress for maximum quality).
 	 */
-	public void takeScreenshot(final String name, final int quality) {
+	public void takeScreenshot(final String dirPath, final String name, final int quality) {
 		View decorView = getScreenshotView();
-		if(decorView == null) 
+		if (decorView == null)
 			return;
 
 		initScreenShotSaver();
-		ScreenshotRunnable runnable = new ScreenshotRunnable(decorView, name, quality);
+		ScreenshotRunnable runnable = new ScreenshotRunnable(decorView, dirPath, name, quality);
 		activityUtils.getCurrentActivity(false).runOnUiThread(runnable);
 	}
 
 	/**
-	 * Takes a screenshot sequence and saves the images with the specified name prefix in "/sdcard/Robotium-Screenshots/". 
-	 *
-	 * The name prefix is appended with "_" + sequence_number for each image in the sequence,
-	 * where numbering starts at 0.  
-	 *
-	 * Requires write permission (android.permission.WRITE_EXTERNAL_STORAGE) in the 
-	 * AndroidManifest.xml of the application under test.
-	 *
-	 * Taking a screenshot will take on the order of 40-100 milliseconds of time on the 
-	 * main UI thread.  Therefore it is possible to mess up the timing of tests if
-	 * the frameDelay value is set too small.
-	 *
-	 * At present multiple simultaneous screenshot sequences are not supported.  
-	 * This method will throw an exception if stopScreenshotSequence() has not been
-	 * called to finish any prior sequences.
-	 *
-	 * @param name the name prefix to give the screenshot
-	 * @param quality the compression rate. From 0 (compress for lowest size) to 100 (compress for maximum quality)
-	 * @param frameDelay the time in milliseconds to wait between each frame
-	 * @param maxFrames the maximum number of frames that will comprise this sequence
-	 *
+	 * Takes a screenshot sequence and saves the images with the specified name
+	 * prefix in "/sdcard/Robotium-Screenshots/".
+	 * 
+	 * The name prefix is appended with "_" + sequence_number for each image in
+	 * the sequence, where numbering starts at 0.
+	 * 
+	 * Requires write permission (android.permission.WRITE_EXTERNAL_STORAGE) in
+	 * the AndroidManifest.xml of the application under test.
+	 * 
+	 * Taking a screenshot will take on the order of 40-100 milliseconds of time
+	 * on the main UI thread. Therefore it is possible to mess up the timing of
+	 * tests if the frameDelay value is set too small.
+	 * 
+	 * At present multiple simultaneous screenshot sequences are not supported.
+	 * This method will throw an exception if stopScreenshotSequence() has not
+	 * been called to finish any prior sequences.
+	 * 
+	 * @param name
+	 *            the name prefix to give the screenshot
+	 * @param quality
+	 *            the compression rate. From 0 (compress for lowest size) to 100
+	 *            (compress for maximum quality)
+	 * @param frameDelay
+	 *            the time in milliseconds to wait between each frame
+	 * @param maxFrames
+	 *            the maximum number of frames that will comprise this sequence
+	 * 
 	 */
 	public void startScreenshotSequence(final String name, final int quality, final int frameDelay, final int maxFrames) {
 		initScreenShotSaver();
 
-		if(screenshotSequenceThread != null) {
+		if (screenshotSequenceThread != null) {
 			throw new RuntimeException("only one screenshot sequence is supported at a time");
 		}
 
@@ -110,28 +125,28 @@ class ScreenshotTaker {
 	/**
 	 * Causes a screenshot sequence to end.
 	 * 
-	 * If this method is not called to end a sequence and a prior sequence is still in 
-	 * progress, startScreenshotSequence() will throw an exception.
+	 * If this method is not called to end a sequence and a prior sequence is
+	 * still in progress, startScreenshotSequence() will throw an exception.
 	 */
 	public void stopScreenshotSequence() {
-		if(screenshotSequenceThread != null) {
+		if (screenshotSequenceThread != null) {
 			screenshotSequenceThread.interrupt();
 			screenshotSequenceThread = null;
 		}
 	}
 
 	/**
-	 * Gets the proper view to use for a screenshot.  
+	 * Gets the proper view to use for a screenshot.
 	 */
 	private View getScreenshotView() {
 		View decorView = viewFetcher.getRecentDecorView(viewFetcher.getWindowDecorViews());
 		final long endTime = SystemClock.uptimeMillis() + Timeout.getSmallTimeout();
 
-		while (decorView == null) {	
+		while (decorView == null) {
 
 			final boolean timedOut = SystemClock.uptimeMillis() > endTime;
 
-			if (timedOut){
+			if (timedOut) {
 				return null;
 			}
 			sleeper.sleepMini();
@@ -150,15 +165,14 @@ class ScreenshotTaker {
 		final CountDownLatch latch = new CountDownLatch(currentViews.size());
 
 		for (GLSurfaceView glView : currentViews) {
-			Object renderContainer = new Reflect(glView).field("mGLThread")
-					.type(GLSurfaceView.class).out(Object.class);
+			Object renderContainer = new Reflect(glView).field("mGLThread").type(GLSurfaceView.class).out(Object.class);
 
 			Renderer renderer = new Reflect(renderContainer).field("mRenderer").out(Renderer.class);
 
 			if (renderer == null) {
 				renderer = new Reflect(glView).field("mRenderer").out(Renderer.class);
 				renderContainer = glView;
-			}  
+			}
 			if (renderer == null) {
 				latch.countDown();
 				continue;
@@ -180,18 +194,18 @@ class ScreenshotTaker {
 		}
 	}
 
-
 	/**
 	 * Returns a bitmap of a given WebView.
-	 *  
-	 * @param webView the webView to save a bitmap from
+	 * 
+	 * @param webView
+	 *            the webView to save a bitmap from
 	 * @return a bitmap of the given web view
 	 * 
 	 */
 
-	private Bitmap getBitmapOfWebView(final WebView webView){
+	private Bitmap getBitmapOfWebView(final WebView webView) {
 		Picture picture = webView.capturePicture();
-		Bitmap b = Bitmap.createBitmap( picture.getWidth(), picture.getHeight(), Bitmap.Config.ARGB_8888);
+		Bitmap b = Bitmap.createBitmap(picture.getWidth(), picture.getHeight(), Bitmap.Config.ARGB_8888);
 		Canvas c = new Canvas(b);
 		picture.draw(c);
 		return b;
@@ -200,44 +214,45 @@ class ScreenshotTaker {
 	/**
 	 * Returns a bitmap of a given View.
 	 * 
-	 * @param view the view to save a bitmap from
+	 * @param view
+	 *            the view to save a bitmap from
 	 * @return a bitmap of the given view
 	 * 
 	 */
 
-	private Bitmap getBitmapOfView(final View view){
+	private Bitmap getBitmapOfView(final View view) {
 		view.destroyDrawingCache();
 		view.buildDrawingCache(false);
 		Bitmap orig = view.getDrawingCache();
 		Bitmap.Config config = null;
 
-		if(orig != null) {
+		if (orig != null) {
 			config = orig.getConfig();
 		}
 
-		if(config == null) {
+		if (config == null) {
 			config = Bitmap.Config.ARGB_8888;
 		}
 		Bitmap b = orig.copy(config, false);
 		view.destroyDrawingCache();
-		return b; 
+		return b;
 	}
 
 	/**
 	 * Returns a proper filename depending on if name is given or not.
 	 * 
-	 * @param name the given name
+	 * @param name
+	 *            the given name
 	 * @return a proper filename depedning on if a name is given or not
 	 * 
 	 */
 
-	private String getFileName(final String name){
+	private String getFileName(final String name) {
 		SimpleDateFormat sdf = new SimpleDateFormat("ddMMyy-hhmmss");
 		String fileName = null;
-		if(name == null){
-			fileName = sdf.format( new Date()).toString()+ ".jpg";
-		}
-		else {
+		if (name == null) {
+			fileName = sdf.format(new Date()).toString() + ".jpg";
+		} else {
 			fileName = name + ".jpg";
 		}
 		return fileName;
@@ -247,16 +262,20 @@ class ScreenshotTaker {
 	 * This method initializes the aysnc screenshot saving logic
 	 */
 	private void initScreenShotSaver() {
-		if(screenShotSaverThread == null || screenShotSaver == null) {
+		if (screenShotSaverThread == null || screenShotSaver == null) {
 			screenShotSaverThread = new HandlerThread("ScreenShotSaver");
 			screenShotSaverThread.start();
 			screenShotSaver = new ScreenShotSaver(screenShotSaverThread);
 		}
 	}
 
-	/** 
-	 * This is the thread which causes a screenshot sequence to happen
-	 * in parallel with testing.
+	public static String defaultScreenshotDir() {
+		return Environment.getExternalStorageDirectory() + "/Robotium-Screenshots/";
+	}
+
+	/**
+	 * This is the thread which causes a screenshot sequence to happen in
+	 * parallel with testing.
 	 */
 	private class ScreenshotSequenceThread extends Thread {
 		private int seqno = 0;
@@ -270,14 +289,15 @@ class ScreenshotTaker {
 
 		public ScreenshotSequenceThread(String _name, int _quality, int _frameDelay, int _maxFrames) {
 			name = _name;
-			quality = _quality; 
+			quality = _quality;
 			frameDelay = _frameDelay;
 			maxFrames = _maxFrames;
 		}
 
 		public void run() {
-			while(seqno < maxFrames) {
-				if(!keepRunning || Thread.interrupted()) break;
+			while (seqno < maxFrames) {
+				if (!keepRunning || Thread.interrupted())
+					break;
 				doScreenshot();
 				seqno++;
 				try {
@@ -290,10 +310,11 @@ class ScreenshotTaker {
 
 		public void doScreenshot() {
 			View v = getScreenshotView();
-			if(v == null) keepRunning = false;
-			String final_name = name+"_"+seqno;
-			ScreenshotRunnable r = new ScreenshotRunnable(v, final_name, quality);
-			Log.d(LOG_TAG, "taking screenshot "+final_name);
+			if (v == null)
+				keepRunning = false;
+			String final_name = name + "_" + seqno;
+			ScreenshotRunnable r = new ScreenshotRunnable(v, defaultScreenshotDir(), final_name, quality);
+			Log.d(LOG_TAG, "taking screenshot " + final_name);
 			activityUtils.getCurrentActivity(false).runOnUiThread(r);
 		}
 
@@ -304,51 +325,55 @@ class ScreenshotTaker {
 	}
 
 	/**
-	 * Here we have a Runnable which is responsible for taking the actual screenshot,
-	 * and then posting the bitmap to a Handler which will save it.
-	 *
+	 * Here we have a Runnable which is responsible for taking the actual
+	 * screenshot, and then posting the bitmap to a Handler which will save it.
+	 * 
 	 * This Runnable is run on the UI thread.
 	 */
 	private class ScreenshotRunnable implements Runnable {
 
 		private View view;
+		private String dirPath;
 		private String name;
 		private int quality;
 
-		public ScreenshotRunnable(final View _view, final String _name, final int _quality) {
+		public ScreenshotRunnable(final View _view, final String _dirPath, final String _name, final int _quality) {
 			view = _view;
 			name = _name;
+			dirPath = _dirPath;
 			quality = _quality;
 		}
 
 		public void run() {
-			if(view !=null){
-				Bitmap  b;
+			if (view != null) {
+				Bitmap b;
 
-				if(view instanceof WebView){
+				if (view instanceof WebView) {
 					b = getBitmapOfWebView((WebView) view);
-				}
-				else{
+				} else {
 					b = getBitmapOfView(view);
 				}
-				if(b != null)
-					screenShotSaver.saveBitmap(b, name, quality);
-				else 
+
+				if (b != null)
+					screenShotSaver.saveBitmap(b, dirPath, name, quality);
+				else
 					Log.d(LOG_TAG, "NULL BITMAP!!");
 			}
 		}
 	}
 
 	/**
-	 * This class is a Handler which deals with saving the screenshots on a separate thread.
-	 *
-	 * The screenshot logic by necessity has to run on the ui thread.  However, in practice
-	 * it seems that saving a screenshot (with quality 100) takes approx twice as long
-	 * as taking it in the first place. 
-	 *
-	 * Saving the screenshots in a separate thread like this will thus make the screenshot
-	 * process approx 3x faster as far as the main thread is concerned.
-	 *
+	 * This class is a Handler which deals with saving the screenshots on a
+	 * separate thread.
+	 * 
+	 * The screenshot logic by necessity has to run on the ui thread. However,
+	 * in practice it seems that saving a screenshot (with quality 100) takes
+	 * approx twice as long as taking it in the first place.
+	 * 
+	 * Saving the screenshots in a separate thread like this will thus make the
+	 * screenshot process approx 3x faster as far as the main thread is
+	 * concerned.
+	 * 
 	 */
 	private class ScreenShotSaver extends Handler {
 		public ScreenShotSaver(HandlerThread thread) {
@@ -357,33 +382,41 @@ class ScreenshotTaker {
 
 		/**
 		 * This method posts a Bitmap with meta-data to the Handler queue.
-		 *
-		 * @param bitmap the bitmap to save
-		 * @param name the name of the file
-		 * @param quality the compression rate. From 0 (compress for lowest size) to 100 (compress for maximum quality).
+		 * 
+		 * @param bitmap
+		 *            the bitmap to save
+		 * @param name
+		 *            the name of the file
+		 * @param quality
+		 *            the compression rate. From 0 (compress for lowest size) to
+		 *            100 (compress for maximum quality).
 		 */
-		public void saveBitmap(Bitmap bitmap, String name, int quality) {
+		public void saveBitmap(Bitmap bitmap, String directory, String name, int quality) {
 			Message message = this.obtainMessage();
 			message.arg1 = quality;
 			message.obj = bitmap;
 			message.getData().putString("name", name);
+			message.getData().putString("dir", directory);
 			this.sendMessage(message);
+
 		}
 
 		/**
 		 * Here we process the Handler queue and save the bitmaps.
-		 *
-		 * @param message A Message containing the bitmap to save, and some metadata.
+		 * 
+		 * @param message
+		 *            A Message containing the bitmap to save, and some
+		 *            metadata.
 		 */
 		public void handleMessage(Message message) {
 			String name = message.getData().getString("name");
+			String dir = message.getData().getString("dir");
 			int quality = message.arg1;
-			Bitmap b = (Bitmap)message.obj;
-			if(b != null) {
-				saveFile(name, b, quality);
+			Bitmap b = (Bitmap) message.obj;
+			if (b != null) {
+				saveFile(dir, name, b, quality);
 				b.recycle();
-			}
-			else {
+			} else {
 				Log.d(LOG_TAG, "NULL BITMAP!!");
 			}
 		}
@@ -391,19 +424,23 @@ class ScreenshotTaker {
 		/**
 		 * Saves a file.
 		 * 
-		 * @param name the name of the file
-		 * @param b the bitmap to save
-		 * @param quality the compression rate. From 0 (compress for lowest size) to 100 (compress for maximum quality).
+		 * @param name
+		 *            the name of the file
+		 * @param b
+		 *            the bitmap to save
+		 * @param quality
+		 *            the compression rate. From 0 (compress for lowest size) to
+		 *            100 (compress for maximum quality).
 		 * 
 		 */
-		private void saveFile(String name, Bitmap b, int quality){
+		private void saveFile(String dirPath, String name, Bitmap b, int quality) {
 			FileOutputStream fos = null;
 			String fileName = getFileName(name);
 
-			File directory = new File(Environment.getExternalStorageDirectory() + "/Robotium-Screenshots/");
-			directory.mkdir();
+			File directory = new File(dirPath);
+			directory.mkdirs();
 
-			File fileToSave = new File(directory,fileName);
+			File fileToSave = new File(directory, fileName);
 			try {
 				fos = new FileOutputStream(fileToSave);
 				if (b.compress(Bitmap.CompressFormat.JPEG, quality, fos) == false)
@@ -411,7 +448,8 @@ class ScreenshotTaker {
 				fos.flush();
 				fos.close();
 			} catch (Exception e) {
-				Log.d(LOG_TAG, "Can't save the screenshot! Requires write permission (android.permission.WRITE_EXTERNAL_STORAGE) in AndroidManifest.xml of the application under test.");
+				Log.d(LOG_TAG,
+						"Can't save the screenshot! Requires write permission (android.permission.WRITE_EXTERNAL_STORAGE) in AndroidManifest.xml of the application under test.");
 				e.printStackTrace();
 			}
 		}
